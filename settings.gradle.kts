a8848eff1a80076ed18950a4c8ea507a3b046a7f
rayl <5249218+rayl6@users.noreply.github.com>
2024-10-22 18:39:06 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        maven("https://maven.aliyun.com/nexus/content/repositories/jcenter")
+        maven("https://maven.aliyun.com/nexus/content/repositories/google")
+        maven("https://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+        maven("https://jitpack.io")
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ComposeDemos"
+include(":app")
